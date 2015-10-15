c2b8924b214aae4b3ac40a6e9dc4a6696cdfed72
danieldealday <danieldealday@gmail.com>
2015-10-15 13:32:17 -0700

    updated with new files

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Authors: Joshua Satterfield, Daniel De Alday
 Date Initiated: October 14, 2015
 Title: Meditation Interval
+on broswer open
+	- start timer
+	- on intervals of 30 minutes open new tab display new page with reminder awesome graphic and animatino to remind user to get up and walk around
+		- time resets and functionality continues until browswer window closes	
---
+++ popup.js
@@ -0,0 +1,57 @@
+var messages = [
+"If it makes you happy, do it. If it doesn\'t, then don\'t.",
+"Be fearless in the pursuit of what sets your soul on fire.",
+"Say what you are thinking, not what you think you should say.",
+"Don\'t hold back.",
+"Nothing to prove. Everything to improve.",
+"Before you find your soulmate, you must first discover your soul.",
+"When I control my mind, I control my life.",
+"Whatever your dream is, every extra penny you have should be going to that.",
+"Do as you wish. Be as you are.",
+"Small changes can make a big difference.",
+"Vulnerability is the only bridge to build connection.",
+"Master the art of observing.",
+"Worth can\'t be measured. It can only be celebrated through growth of what you\'ve become or whatever else you with to mold into.",
+"I thought about quitting then I noticed who was watching.",
+"Just keep moving forward and don\'t give a shit about what anybody thinks. Do what you have to do for you. - Johnny Depp",
+"Life isn\'t about finding yourself. Life is about creating yourself.",
+"The most memorable moments in life are the ones you never planned.",
+"The more that you read, the more things you will know. The more that you learn, the more places you\'ll go.",
+"The price of greatness is responsibility.",
+"Life only comes around once, so do whatever makes you happy, and be with whoever makes you smile.",
+"Be who you needed when you were younger.",
+"Never lose yourself loving someone else. Always remember who you are.",
+"I may not be there yet, but I\'m closer than I was yesterday.",
+"The things you regret most in life are the risks you didn\'t take.",
+"Happiness is all about loving what you have and being grateful for it. Never lose sight of your blessings.",
+"You have to go through the worst to get to the best.",
+"It doesn\'t matter how fast of how slow you go. Just don\'t stop.",
+"Never lose hope. You never know what tomorrow may bring. There is always a chance to change and make things right.",
+"Be so good they can\'t ignore you. - Steve Martin",
+"Always end the day with a positive thought. No matter how hard things were, tomorrow is a fresh opportunity to make it better.",
+"Happiness isn\'t about getting what you want all the time. It\'s about loving what you have and being grateful for it.",
+"I will never apologize for being me. You should apologize for asking me to be anything else.",
+"Whatever you decide to do, make sure it makes you happy.",
+"Have a clear purpose.",
+"Don\'t change to please people. If they can\'t accept you for who you are, you don\'t need them in your life.",
+"it\'s all about looking forward and being excited about whatever it is you have planned for your future.",
+"People aren\'t always going to be there for you, that\'s why you gotta learn to handle things on your own.",
+"Good judgement comes from experience. A lot of experience comes from bad judgement.",
+"Life is better when you\'re laughing.",
+"It\'s not the speed that matters. Its the direction.",
+"If you\'re still looking for for that one person who can change your life, take a look in the mirror.",
+"Set some goals and do something positive with your life.",
+"If you\'re lucky enough to get a second chance at something, don\'t waste it.",
+"You have to be odd to be number one. - Dr. Seuss",
+"Work like most people won\'t so you can live like most people can\'t."]
+
+function displayMessage () {
+  var quote = document.createElement("div");
+  quote.innerHTML = '<h1>"' + messages[Math.ceil(Math.random()*10)] + '"</h1>';
+  document.body.appendChild(quote);
+}
+displayMessage();
+
+function startClock () {
+
+}
